a6394fb241111c80afaa7d7da05b7e94f97e6576
ADHITHYA M <97330760+Adhi-Git-hub@users.noreply.github.com>
2024-08-24 00:06:46 +0530

    loadmodel.py

--- wordembeededmodel/loadmodel.py
+++ wordembeededmodel/loadmodel.py
@@ -5,7 +5,7 @@ from gensim.models import KeyedVectors
 start_time = time.time()
 
 # Load the binary model
-word_vectors = KeyedVectors.load_word2vec_format('C:\\Users\\adhit\\Documents\\projects\\badwordsproject\\finalmodel\\tamil_WE_SG.bin', binary=True)
+word_vectors = KeyedVectors.load_word2vec_format('path\\finalmodel\\tamil_WE_SG.bin', binary=True)
 
 # Stop the timer
 end_time = time.time()
@@ -13,9 +13,7 @@ end_time = time.time()
 # Calculate loading time
 loading_time = end_time - start_time
 print(f"Model loading completed in {loading_time:.2f} seconds")
-# %%
+
 words = word_vectors.most_similar('உலகில்')
 print(words)
 
-
-# %%
